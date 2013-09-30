740ad7fe3b3467a00bf69837e8a384c6aa7f56c8
Kegan Quimby <kegan.quimby@gmail.com>
2013-09-29 22:32:37 -0400

    js quirk fixed

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,11 +1,11 @@
 jQuery( document ).ready(function($) {
     $('button#headerToggleNav').click(function(){
     	$('.main-container').animate({ margin: "0 0 0 -306px" }, "slow");
-    	$('.main-nav').addClass('open');
+    	$('.main-nav').animate({ margin: "0" }, "slow");
     });
 
     $('button#navCloseBtn').click(function(){   
     	$('.main-container').animate({ margin: "0" }, "slow");
-    	$('.main-nav').removeClass('open');
+    	$('.main-nav').animate({ margin: "0 -400px 0 0" }, "slow");
     });
 });
