f24b166a47b33a77342e2d5b2f9fad4a5848413d
Kegan Quimby <kegan.quimby@gmail.com>
2013-10-01 22:14:09 -0700

    Revert "old changes"
    This reverts commit dc3f7486af93a560a5f03ae58246b4fed4a25764.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,11 @@
 jQuery( document ).ready(function($) {
     $('button#headerToggleNav').click(function(){
     	$('.main-container').animate({ margin: "0 0 0 -306px" }, "slow");
-    	$('.main-nav').animate({ margin: "0" }, "slow");
-
+    	$('.main-nav').addClass('open');
     });
 
     $('button#navCloseBtn').click(function(){   
     	$('.main-container').animate({ margin: "0" }, "slow");
-    	$('.main-nav').animate({ margin: "0 -400px 0 0" }, "slow");
+    	$('.main-nav').removeClass('open');
     });
 });
